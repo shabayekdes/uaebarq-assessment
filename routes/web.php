0c424b3b4a6f49897eeed9432b3cc92af5760cdb
Esmail Shabayek <esmail.shabayek@gmail.com>
2020-06-11 12:59:07 +0200

    Fix on admin dashboard

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('admin/{path?}', 'Admin\DashboardController@index')->name('admin')->where('path', '.*');
+});
--- webpack.mix.js
+++ webpack.mix.js
@@ -10,6 +10,16 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
+mix.webpackConfig({
+    resolve: {
+        alias: {
+            "@Admin": path.resolve(
+                __dirname,
+                "resources/js/"
+            )
+        }
+    }
+});
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
